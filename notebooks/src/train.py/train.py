2c14ecd4a092ce01faa1d8c9cbcc0af0951cf76b
amalbabujnvpkd-bit <amalbabujnvpkd@gmail.com>
2026-09-26 07:48:20 +0530

    Update train.py

--- notebooks/src/train.py/train.py
+++ notebooks/src/train.py/train.py
@@ -1,3 +1,18 @@
 import os
 import pandas as pd
 DATA_PATH = "../data/aclImdb"
+def load_reviews(folder_path, label):
+    reviews = []
+
+    for filename in os.listdir(folder_path):
+        file_path = os.path.join(folder_path, filename)
+
+        with open(file_path, "r", encoding="utf-8") as file:
+            text = file.read()
+
+        reviews.append({
+            "review": text,
+            "sentiment": label
+        })
+
+    return reviews
